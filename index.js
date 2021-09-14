836d0ba70261203e472868b7f2fa2d092c6743d7
Samuel <samlee1097@yahoo.com>
2021-09-14 12:08:40 -0400

    added star feature to reviews

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function displayGames() {
         .then(res => res.json())
         .then(function(data) {
             data.forEach(element => {
-                console.log()
                 renderGameRow(element)
             });
         })
@@ -18,46 +17,41 @@ function renderGameRow(e) {
     const commentTitle = mkElement('summary')
     commentTitle.innerText = "Reviews"
     reviewDetails.appendChild(commentTitle)
-    // e.reviews.forEach( function(r) {
-    //     const review = mkElement('p')
-    //     review.innerText = `(${r.rating}*) - ${r.comment}`
-    //     reviewDetails.appendChild(review)
-    // })
-        const review = mkElement('p') // temporary
-        review.innerText = "(RATING HERE) - (COMMENT HERE)" // temporary
-        reviewDetails.appendChild(review) // temporary
 
-    const thumbnailCell = mkElement('td')
+    e.reviews.forEach( function(r) {
+        const review = mkElement('p')
+        review.innerText = ""
+        for (let i=0; i<5; i++){
+            if (i <= r.rating){
+                review.innerText += `★`;
+            } else {
+                review.innerText += `☆`;
+            }
+        }
+        review.innerText += ` - ${r.comment}`
+        reviewDetails.appendChild(review)
+    })
+
     const nameCell = mkElement('td')
     const releaseCell = mkElement('td')
     const genreCell = mkElement('td')
     const ratingCell = mkElement('td')
     const reviewCell = mkElement('td')
 
-    const thumbnailImg = mkElement('img')
-    thumbnailImg.src = e.image
-    thumbnailImg.alt = e.name
-    thumbnailImg.className = "thumbnail"
-    thumbnailCell.appendChild(thumbnailImg)
-
     nameCell.innerText = e.name
     releaseCell.innerText = e.release
-    genreCell.innerText = e.genre
-    // ratingCell.innerText = `(${calculateRating(e)}*)`
-        ratingCell.innerText = "*****" // temporary
+    genreCell.innerText = e.genre 
+    ratingCell.innerText = `(${calculateRating(e)}*)`
     reviewCell.append(reviewDetails)
-    
 
-    // console.log(ratingCell.innerText)
 
-    gameRow.append(thumbnailCell, nameCell, releaseCell, genreCell, ratingCell, reviewCell)
+    gameRow.append(nameCell, releaseCell, genreCell, ratingCell, reviewCell)
     gameTable.appendChild(gameRow)
 }
 
 function calculateRating(e) {
     const numberOfReviews = e.reviews.length
     let ratingTally = 0
-
     for (let i = 0; i < numberOfReviews; i++) {
         ratingTally += e.reviews[i].rating
     }
@@ -70,7 +64,6 @@ const mkElement = (element) => document.createElement(element)
 function init () {
     displayGames()
 }
-
 
 BASE_URL = "http://localhost:3000/games?_embed=reviews"
 init()
